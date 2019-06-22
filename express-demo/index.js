4600b06f44ccc2a18dd0a654b2a350b8f94855da
Meowsers25 <c.lamothe25@gmail.com>
2019-06-22 13:15:14 -0400

    Starting first Express app

---
+++ express-demo/index.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const app = express()
+
+// HTTP verbs
+app.get('/', (req, res) => {
+    res.send('Hello, Express!!!! :0')
+})
+
+app.get('/api/courses', (req, res) => {
+    res.send([5, 8, 25])
+})
+
+const port = process.env.PORT || 3000
+
+app.listen(3000, () => console.log(`Listening on port ${port}....`))
