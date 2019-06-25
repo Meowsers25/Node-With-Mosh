59f48c3978df0185c4e7e63930f2b484628ab32b
Meowsers25 <c.lamothe25@gmail.com>
2019-06-24 21:31:43 -0400

    Working with PUT requests

--- express-demo/index.js
+++ express-demo/index.js
@@ -37,24 +37,32 @@ app.get('/api/courses/:id', (req, res) => {
 // post course
 app.post('/api/courses', (req, res) => {
     // define schema object to utilize Joi
-    const schema = {
-        // check Joi docs for various methods
-        name: Joi.string().min(3).required()
-    }
+    // const schema = {
+    //     // check Joi docs for various methods
+    //     name: Joi.string().min(3).required()
+    // }
 
-    // validate body and schema
-    const result = Joi.validate(req.body, schema)
-    // console.log(result)
+    // // validate body and schema
+    // const result = Joi.validate(req.body, schema)
+    // // console.log(result)
 
-    // check input validation
-    // if(!req.body.name || req.body.name.length < 3){
-    if(result.error){
-        //status 400
-        // res.status(400).send('Incorrect naming convention. Status 400')
-        res.status(400).send(result.error.details[0].message)
-        // return to exit code
+    // // check input validation
+    // // if(!req.body.name || req.body.name.length < 3){
+    // if(result.error){
+    //     //status 400
+    //     // res.status(400).send('Incorrect naming convention. Status 400')
+    //     res.status(400).send(result.error.details[0].message)
+    //     // return to exit code
+    //     return
+    // }
+
+    const { error } = validateCourse(req.body)
+    
+    if (error){
+        res.status(400).send(error.details[0].message)
         return
     }
+
     const course = {
         id: courses.length + 1,
         name: req.body.name
@@ -62,6 +70,34 @@ app.post('/api/courses', (req, res) => {
     courses.push(course)
     res.send(course)
 })
+
+app.put('/api/courses/:id', (req, res) => {
+    // Look up course
+    // !exist, return 404
+    const course = courses.find(c => c.id === parseInt(req.params.id))
+    if (!course) res.status(404).send('No course with thad ID')
+
+    //validate
+    // !valid, return 400 - bad request
+    const { error } = validateCourse(req.body)
+    
+    if (error){
+        res.status(400).send(error.details[0].message)
+        return
+    }
+
+    // Update course
+    // return updated course
+    course.name = req.body.name
+    res.send(course)
+})
+
+function validateCourse(course){
+    const schema = {
+        name: Joi.string().min(3).required()
+    }
+    return Joi.validate(course, schema)
+}
 
 // environment variable
 const port = process.env.PORT || 3000
